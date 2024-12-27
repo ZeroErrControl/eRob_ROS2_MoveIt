b23de95653d84a9b815c47ca78840211262b4798
ZeroErrControl <overseas_dpt@zeroerr.cn>
2024-12-27 09:57:04 +0800

    Force entering DC mode and add a timeout.

--- twojoints_test/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zero/IGH_EtherCAT/eRob_arm/src/twojoints_test
-# Build directory: /home/zero/IGH_EtherCAT/eRob_arm/src/twojoints_test/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
